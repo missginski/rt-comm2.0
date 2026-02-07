88a4467093611a6566185144de72eba743f15232
Kristyn Siedenburg <missginski@gmail.com>
2026-02-07 13:14:10 -0500

    add SEO properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,27 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              name: "Real-Time Communications",
+              url: "https://real-timecommunications.com",
+              telephone: "+1-908-670-4934",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "175 Stanhope Sparta Rd, Blg 3 Unit 3",
+                addressLocality: "Andover",
+                addressRegion: "NJ",
+                postalCode: "07821",
+                addressCountry: "US"
+              }
+            }),
+          }}
+        />
       </body>
     </html>
   );
